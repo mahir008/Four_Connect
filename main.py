8ec0c9b80c9532191a43df964f181003c6ed3625
mahir <mahirul.lasker@northsouth.edu>
2022-05-13 23:57:18 +0600

    command line layout updated with numpy and functions

--- main.py
+++ main.py
@@ -1,32 +1,66 @@
 import numpy as np
+import random
+import pygame
+import sys
+import math
 
- def create_board(board,col):
-     board = np.zeros((6,7))
-     return board
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
-
- def pieces_to_be_dropped():
- pass
-
-
- def is_no_in_valid_location():
-     pass
-
- def get_next_open_row():
- pass
+no_of_rows = 6
+no_of_columns = 7
 
 
- board = create_board()
- game_over = False
- turn=0
- while not game_over:
-     # Ask for player 1 input
- if turn == 0:
-     col= int(input("Player 1 make your selection (0-6):"))
+def board_create():
+    board = np.zeros((no_of_rows, no_of_columns))
+    return board
 
-     #ask for player 2 input
- else:
-     col = int(input("Player 2 make your selection (0-6):"))
 
-                     turn +=1
-                     turn= turn % 2
+def piece_drop(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[no_of_rows - 1][col] == 0
+
+
+def next_open_row(board, col):
+    for r in range(no_of_rows):
+        if board[r][col] == 0:
+            return r
+
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+def winner_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(no_of_columns - 3):
+        for r in range(no_of_rows):
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
+                c + 3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for c in range(no_of_columns):
+        for r in range(no_of_rows - 3):
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
+                c] == piece:
+                return True
+
+    # Check positively sloped diaganols
+    for c in range(no_of_columns - 3):
+        for r in range(no_of_rows - 3):
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
+                c + 3] == piece:
+                return True
+
+    # Check negatively sloped diaganols
+    for c in range(no_of_columns - 3):
+        for r in range(3, no_of_rows):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
+                c + 3] == piece:
+                return True
