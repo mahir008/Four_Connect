7bde29f11eb0254828ffc086e89a3dfa0ea1d551
mahir <mahirul.lasker@northsouth.edu>
2022-05-14 00:12:23 +0600

    players input updated

--- main.py
+++ main.py
@@ -124,3 +124,40 @@ while not game_over:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
             # print(event.pos)
+ # Ask for Player 1 Input
+            if turn == 0:
+                posx = event.pos[0]
+                col = int(math.floor(posx / SQUARESIZE))
+
+                if is_valid_location(board, col):
+                    row = next_open_row(board, col)
+                    piece_drop(board, row, col, 1)
+
+                    if winner_move(board, 1):
+                        label = myfont.render("PLAYER 1 WINNER", 1, RED)
+                        screen.blit(label, (40, 10))
+                        game_over = True
+
+
+            # # Ask for Player 2 Input
+            else:
+                posx = event.pos[0]
+                col = int(math.floor(posx / SQUARESIZE))
+
+                if is_valid_location(board, col):
+                    row = next_open_row(board, col)
+                    piece_drop(board, row, col, 2)
+
+                    if winner_move(board, 2):
+                        label = myfont.render("PLAYER 2 WINNER", 1, YELLOW)
+                        screen.blit(label, (40, 10))
+                        game_over = True
+
+            print_board(board)
+            draw_board(board)
+
+            turn += 1
+            turn = turn % 2
+
+            if game_over:
+                pygame.time.wait(3000)
